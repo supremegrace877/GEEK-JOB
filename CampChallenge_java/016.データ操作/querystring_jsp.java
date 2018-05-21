5bf069d30d2e004899f830b2b9866600a320ecbb
大西はるか <supremegrace8777@gmail.com>
2018-05-21 16:31:00 +0900

    Java-データ操作-クエリストリングの利用課題提出します

---
+++ CampChallenge_java/016.データ操作/querystring_jsp.java
@@ -0,0 +1,93 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class querystring_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      response.setHeader("X-Powered-By", "JSP/2.3");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        ");
+
+            request.setCharacterEncoding("UTF-8");
+            request.getParameter("total");
+            
+            request.getParameter("count");
+            
+            out.print((request.getParameter("syubetsu"))+"を購入しました。<BR>");
+            //受け取ったデータは文字列なのでint型に型変換する
+            int tanka = (Integer.parseInt(request.getParameter("total"))) / (Integer.parseInt(request.getParameter("count")));
+            out.print("単価は"+tanka+"円です。<br>");
+            int sogaku = (Integer.parseInt(request.getParameter("total")));
+            int point;
+            if (sogaku < 3000) {
+                out.print("0ポイントです。");
+            } else if (3000 <= sogaku && sogaku < 5000) {
+                point = (int)Math.floor(sogaku * 0.04);
+                out.print(point+"ポイントです。");
+            } else if (5000 <= sogaku) {
+                point = (int)Math.floor(sogaku * 0.05);
+                out.print(point+"ポイントです。");
+            }
+        
+      out.write("\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
